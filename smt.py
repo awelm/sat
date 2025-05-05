c93e46d48701c72c43eb8f4a2e73b07ea14174c9
Akila Welihinda <awelm@openai.com>
2025-05-05 03:02:33 -0700

    fixed

--- smt.py
+++ smt.py
@@ -1,69 +1,77 @@
-from z3 import *
+from z3 import Bool, Int, Optimize, Sum, If, And, sat
 
-# Given an adjacency matrix describing a graph, return the minimum cost and route that starts at city 0, visits each
-# city exactly once and returns to city 0
+
+# Given an adjacency matrix describing a graph, return the minimum cost and route that
+# starts at city 0, visits each city exactly once and returns to city 0.
 def smt(distances):
     num_cities = len(distances)
     s = Optimize()
 
-    # Variables representing our decision to use an edge or not
-    edges_used = [[Bool("r_%s_%s" % (i, j)) for j in range(num_cities)] for i in range(num_cities)]
+    # Variables representing our decision to use an edge or not.
+    edges_used = [[Bool(f"r_{i}_{j}") for j in range(num_cities)] for i in range(num_cities)]
 
-    # Variable representing the total distance traveled. If we use an edge, then its distance
-    # is added to total_distance
+    # Variable representing the total distance traveled. If we use an edge then its
+    # distance is added to total_distance.
     total_distance = Int("total_distance")
-    s.add(total_distance == Sum(
-        [If(edges_used[i][j], distances[i][j], 0) for i in range(num_cities) for j in range(num_cities)]))
+    s.add(
+        total_distance
+        == Sum(
+            [
+                If(edges_used[i][j], distances[i][j], 0)
+                for i in range(num_cities)
+                for j in range(num_cities)
+            ]
+        )
+    )
 
-    # Variables representing the order in which cities are visited. This will help us eliminate
-    # subtours by applying the MTZ constraint (explained below)
-    orders = [Int("order_%s" % i) for i in range(num_cities)]
-    for i in range(num_cities):
-        s.add(orders[i] >= 0, orders[i] < num_cities)
+    # Variables representing the order in which cities are visited. This will help us
+    # eliminate subtours by applying the MTZ constraint (explained below).
+    orders = [Int(f"order_{i}") for i in range(num_cities)]
+    s.add(orders[0] == 0)
+    for i in range(1, num_cities):
+        s.add(And(orders[i] >= 1, orders[i] < num_cities))
 
-    # Add constraint that binds edges_used with the order in which cities are visited
+    # Add constraint that binds edges_used with the order in which cities are visited.
     for i in range(num_cities):
-        for j in range(num_cities):
+        s.add(edges_used[i][i] == False)
+        s.add(Sum([If(edges_used[i][j], 1, 0) for j in range(num_cities)]) == 1)
+
+    # Add constraint to ensure that each city in the tour has only one predecessor and
+    # one successor. This can be enforced by ensuring the sum of each row and column in
+    # the edges_used matrix is 1.
+    for j in range(num_cities):
+        s.add(Sum([If(edges_used[i][j], 1, 0) for i in range(num_cities)]) == 1)
+
+    for i in range(1, num_cities):
+        for j in range(1, num_cities):
             if i != j:
-                # Apply MTZ constraint to eliminate subtours from our solution. All this means is that if we use an edge
-                # from city i -> city j, then city j must be visited after city i. This also ensures that city 0 is
-                # visited first
-                if j != 0:
-                    s.add(If(edges_used[i][j] == True, orders[j] > orders[i], True))
-            else:
-                # You can't travel from a city to itself
-                s.add(edges_used[i][j] == False)
+                s.add(
+                    orders[i]
+                    - orders[j]
+                    + (num_cities - 1) * If(edges_used[i][j], 1, 0)
+                    <= num_cities - 2
+                )
 
-    # Add constraint to ensure that each city in the tour has only one predecessor and one successor. This can be
-    # enforced by ensuring the sum of each row and column in the `edges_used` matrix is 1. Use Bools instead of
-    # Integers to encode this constraint more efficiently
-    for i in range(num_cities):
-        visited = False
-        for j in range(num_cities):
-            s.add(If(edges_used[i][j] == True, visited == False, True))
-            visited = Or(visited, edges_used[i][j])
-        s.add(visited == True)
-
-    # Call the solver and return the minimum cost and tour path
+    # Call the solver and return the minimum cost and tour path.
     s.minimize(total_distance)
+
     if s.check() == sat:
         model = s.model()
-        min_tour_cost = model[total_distance].as_long()
-        return min_tour_cost, get_tour_path(s, 0, num_cities, edges_used)
-    else:
-        return -1, []
+        cost = model[total_distance].as_long()
+        return cost, get_tour_path(s, 0, num_cities, edges_used)
+
+    return -1, []
 
 
-# Return the optimal tour path found by the solver
+# Return the optimal tour path found by the solver.
 def get_tour_path(s, start_city, num_cities, routes):
     model = s.model()
     curr_city = start_city
     path = []
-    for i in range(num_cities):
+    for _ in range(num_cities):
+        path.append(curr_city)
         for j in range(num_cities):
             if model[routes[curr_city][j]]:
-                path.append(curr_city)
                 curr_city = j
                 break
-
     return path + [start_city]
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from smt import smt
-from brute import brute
+from dp import dp
 import random
 import time
 import matplotlib.pyplot as plt
@@ -13,16 +13,16 @@ distances = [
 ]
 '''
 
-max_size = 9
+max_size = 18
 smt_enabled = True
-brute_enabled = False
+dp_enabled = True
 
 smt_sizes = []
 smt_times = []
-brute_sizes = []
-brute_times = []
+dp_sizes = []
+dp_times = []
 
-for _ in range(300):
+for _ in range(100):
     size = random.randint(2, max_size)
     distances = [[random.randint(0, 100) for _ in range(size)] for _ in range(size)]
     for i in range(size):
@@ -36,23 +36,26 @@ for _ in range(300):
         print(f"smt took {smt_time}")
         smt_sizes.append(size)
         smt_times.append(smt_time)
-    if brute_enabled:
+    if dp_enabled:
         start = time.perf_counter()
-        brute_dist, brute_path = brute(distances, 0)
-        brute_time = time.perf_counter() - start
-        print(f"brute took {brute_time}")
-        print(f"brute: {brute_dist}, {brute_path}")
-        brute_sizes.append(size)
-        brute_times.append(brute_time)
-    if smt_enabled and brute_enabled:
-        if smt_dist == brute_dist and (smt_path == brute_path or smt_path == brute_path[::-1]):
+        dp_dist, dp_path = dp(distances)
+        dp_time = time.perf_counter() - start
+        print(f"dp took {dp_time}")
+        print(f"dp: {dp_dist}, {dp_path}")
+        dp_sizes.append(size)
+        dp_times.append(dp_time)
+    if smt_enabled and dp_enabled:
+        if smt_dist == dp_dist:
             print("Agree")
         else:
             print("DISAGREE")
 
+print("smt sizes and times:", list(zip(smt_sizes, smt_times))[-10:])
+print("dp sizes and times:", list(zip(dp_sizes, dp_times))[-10:])
 
-plt.scatter(brute_sizes, brute_times, label="brute")
+plt.scatter(dp_sizes, dp_times, label="dp")
 plt.scatter(smt_sizes, smt_times, label="smt")
 plt.xlabel('Cities')
 plt.ylabel('Time (seconds)')
+plt.legend()
 plt.show()
